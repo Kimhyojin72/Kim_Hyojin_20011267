2d787ef5f57467e6c941bfc0405d4aab5d2f62ae
김효진 <@172.16.100.20>
2023-05-22 23:19:29 +0900

    PQW2ECI

---
+++ PQW2ECI.m
@@ -0,0 +1,13 @@
+function DCM=PQW2ECI(arg_prg,inc_angle,RAAN);
+
+arg_prg_matrix=[cos(arg_prg) -sin(arg_prg) 0 ;
+    sin(arg_prg) cos(arg_prg) 0;
+    0 0 1];
+inc_angle_matrix=[1 0 0 ;
+    0 cos(inc_angle) -sin(inc_angle) ;
+    0 sin(inc_angle) cos(inc_angle)];
+RAAN_matrix=[ cos(RAAN) -sin(RAAN) 0 ;
+    sin(RAAN) cos(RAAN) 0;
+    0 0 1];
+
+DCM=transpose(arg_prg_matrix*inc_angle_matrix*RAAN_matrix);
