43b86fd6a387fb7db12eae1c12c3c52f621ba420
김효진 <@172.16.100.20>
2023-06-11 03:15:09 +0900

    assignment12

--- solveRangeInPerifocalFrame.m
+++ solveRangeInPerifocalFrame.m
@@ -3,7 +3,10 @@
 
 function rangeInPQW = solveRangeInPerifocalFrame(semimajor_axis,eccentricity,true_anomaly)
 
-r = (semimajor_axis*(1-eccentricity^2))/(1+eccentricity*cos(true_anomaly));
-rangeInPQW = [r * cos(true_anomaly) r*sin(true_anomaly) 0]';
+true_anomaly_rad = pi/180 * true_anomaly;
+
+r = semimajor_axis*(1-eccentricity^2)/(1+eccentricity*cos(true_anomaly_rad));
+r_sn = [r; 0; 0];
+rangeInPQW = [cos(true_anomaly_rad) -sin(true_anomaly_rad) 0; sin(true_anomaly_rad) cos(true_anomaly_rad) 0; 0 0 1]*r_sn;
 
 end
--- solveVelocityInPerifocalFrame.m
+++ solveVelocityInPerifocalFrame.m
@@ -3,8 +3,10 @@
 
 function velocityInPQW = solveVelocityInPerifocalFrame(semimajor_axis,eccentricity,true_anomaly)
 
+true_anomaly_rad = true_anomaly * pi/180;
+
 mu = 3.986004418*10^14; %[m^3s^(-2)]
 
-velocityInPQW = sqrt(mu/(semimajor_axis(1-eccentricity^2)))*[-sin(true_anomaly); eccentricity+cos(true_anomaly); 0];
+velocityInPQW = sqrt(mu/(semimajor_axis*(1-eccentricity^2))).*[-sin(true_anomaly_rad); eccentricity+cos(true_anomaly_rad); 0];
 
 end
